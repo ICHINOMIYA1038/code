b1e43f3b232ceff04f5abfe7483203984f3662df
ICHINOMIYA1038 <ichiryo108@gmail.com>
2023-06-08 01:40:47 +0000

    test

--- app/main.go
+++ app/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World!!") //GitOpsの実行時に変更する箇所
+	fmt.Fprintf(w, "Hello world!!!") //GitOpsの実行時に変更する箇所
 }
 
 func main() {
